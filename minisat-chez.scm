f9cfd503a14e6296033f3a72ddede209e8fa0333
Nada Amin <namin@seas.harvard.edu>
2020-03-14 16:54:32 -0400

    minisat interface

--- minisat-chez.scm
+++ minisat-chez.scm
@@ -2,6 +2,17 @@
 
 (define minisat_new (foreign-procedure "minisat_new" () uptr))
 
-(define minisat_delete (foreign-procedure "minisat_delete" (uptr) void))
+(define minisat_newLit (foreign-procedure "minisat_newLit" (uptr) int))
+(define minisat_negate (foreign-procedure "minisat_negate" (int) int))
 
-  
+(define minisat_addClause_begin (foreign-procedure "minisat_addClause_begin" (uptr) void))
+(define minisat_addClause_addLit (foreign-procedure "minisat_addClause_addLit" (uptr int) void))
+(define minisat_addClause_commit (foreign-procedure "minisat_addClause_commit" (uptr) boolean))
+
+(define minisat_solve_begin (foreign-procedure "minisat_solve_begin" (uptr) void))
+(define minisat_solve_addLit (foreign-procedure "minisat_solve_addLit" (uptr int) void))
+(define minisat_solve_commit (foreign-procedure "minisat_solve_commit" (uptr) boolean))
+
+(define minisat_okay (foreign-procedure "minisat_okay" (uptr) boolean))
+
+(define minisat_delete (foreign-procedure "minisat_delete" (uptr) void))
---
+++ minisat.scm
@@ -0,0 +1,62 @@
+(define-structure (minisat-solver ptr))
+(define-structure (minisat-var id))
+(define-structure (minisat-lit id))
+
+(define (check-minisat-solver s proc)
+  (unless (minisat-solver? s)
+    (error proc "not a solver" s)))
+
+(define (check-minisat-var v proc)
+  (unless (minisat-var? v)
+    (error proc "not a var" v)))
+
+(define (check-minisat-lit v proc)
+  (unless (minisat-lit? v)
+    (error proc "not a lit" v)))
+
+(define (sat/new)
+  (make-minisat-solver (minisat_new)))
+
+;; TODO: can we integrate with garbage collection so that we don't
+;;   have to manually delete?
+(define (sat/delete s)
+  (check-minisat-solver s 'sat/delete)
+  (minisat_delete (minisat-solver-ptr s)))
+
+(define (sat/new-var s)
+  (check-minisat-solver s 'sat/new-var)
+  (make-minisat-var (minisat_newLit (minisat-solver-ptr s))))
+
+(define (sat/pos v)
+  (check-minisat-var v 'sat/pos)
+  (make-minisat-lit (minisat-var-id v)))
+(define (sat/neg v)
+  (check-minisat-var v 'sat/neg)
+  (make-minisat-lit (minisat_negate (minisat-var-id v))))
+
+(define (sat/add-clause! s ls)
+  (check-minisat-solver s 'sat/add-clause!)
+  (let ((sp (minisat-solver-ptr s)))
+    (minisat_addClause_begin sp)
+    (for-each
+     (lambda (l)
+       (check-minisat-lit l 'sat/add-clause!)
+       (minisat_addClause_addLit sp (minisat-lit-id l)))
+     ls)
+    (minisat_addClause_commit sp)
+    (void)))
+
+(define (sat/check-sat-assuming s ls)
+  (check-minisat-solver s 'sat/check-sat-assuming)
+  (let ((sp (minisat-solver-ptr s)))
+    (minisat_solve_begin sp)
+    (for-each
+     (lambda (l)
+       (check-minisat-lit l 'sat/check-sat-assuming)
+       (minisat_solve_addLit sp (minisat-lit-id l)))
+     ls)
+    (minisat_solve_commit sp)))
+
+
+
+
